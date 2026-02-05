d8c55ae3d78425b7b18cb703e47cda4aea31328a
kawshal <111242719+kawshal43@users.noreply.github.com>
2026-02-06 02:08:33 +0530

    Update project code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import AppRoutes from "./routes";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <div className="main-content">
-        <AppRoutes />
+        <Home />
+        <Services />
+        <About />
+        <Contact />
       </div>
       <Footer />
     </>
